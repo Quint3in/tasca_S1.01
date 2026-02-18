54c9274b57ab727ba40257dbf6627a2182166f5d
JoelQuintana <joelqn95@gmail.com>
2026-02-18 10:35:41 +0100

    feat(main): add Main class to instantiate and run instrument subclasses

--- Exercici1/src/Main.java
+++ Exercici1/src/Main.java
@@ -1,1 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
 
+        PercussionInstrument drum = new PercussionInstrument();
+        StringInstrument violin = new StringInstrument();
+        WindInstrument flute = new WindInstrument();
+
+        drum.play();
+        violin.play();
+        flute.play();
+    }
+}
